48e62bbe3cf444d757bf15eed4ead7df1d0d21b0
jorge <toxicpotato1223@gmail.com>
2025-01-14 22:09:36 +0800

    Made random index generator for index

--- src/test/eventStore.test.js
+++ src/test/eventStore.test.js
@@ -4,9 +4,11 @@ import { MineSweeper } from "../GameScripts/Game";
 
 describe('EventStore', () => {
     test('Check if tile uncovered event store is successfully store the event', () => {
-        const id = MineSweeper.TilePressed({ index: 1 });
+        const randomIndex = Math.floor(Math.random() * 100);
+        const id = MineSweeper.TilePressed({ index: randomIndex });
         const Event = EventStore.getSpecificEvent({ EventId: id });
-        console.log(`Event ID: ${id}`)
+        console.log(`Random Index: ${randomIndex}`);
+        console.log(`Event ID: ${id}`);
         console.log("Event Found:", Event);
 
         expect(Event).not.toBeNull();
