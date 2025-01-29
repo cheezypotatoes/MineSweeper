ef8d021b36a9a12d497c2f08d63c493c0a959523
jorge <toxicpotato1223@gmail.com>
2025-01-29 18:48:48 +0800

    Made event bootstrap cleaner by categorizing functions

--- src/GameScripts/EventBus/EventBootStrap.js
+++ src/GameScripts/EventBus/EventBootStrap.js
@@ -4,71 +4,44 @@ import { ProjectionManager } from "../EventStore/ProjectionManager";
 
 const BootStrapList = new Map();
 
-const TilePressed = ({ index }) => {
-    MineSweeper.TilePressed({ index: index });
-};
 
-const CreateFlagEvent = ({ index }) => {
-    MineSweeper.CreateFlagEvent({ index: index });
-}
+// MineSweeper functions
+const TilePressed = ({ index }) => {MineSweeper.TilePressed({ index: index });};
+const CreateFlagEvent = ({ index }) => {MineSweeper.CreateFlagEvent({ index: index });};
+const SetBoardSize = ({ height, width }) => {MineSweeper.setHeightWidth({ height: height, width: width }) };
+const CreateEventForTilesWithZeroAdjacentCheckQueue = () => {MineSweeper.createEventForTilesWithZeroAdjacentCheckQueue();};
+const GenerateBombs = ({ amount }) => {return MineSweeper.GenerateBomb({amount: amount});}
+const CheckIfIndexIsBomb = ({ index }) => {return MineSweeper.isTileUncoveredBomb({ index:index })};
+const GetAdjacentNumber = ({ index }) => {return MineSweeper.getAdjacentNumber({ index: index });};
+const returnGameStatus = () => {return MineSweeper.returnGameStatus();};
+BootStrapList.set("TilePressed", TilePressed);
+BootStrapList.set("CreateFlagEvent", CreateFlagEvent);
+BootStrapList.set("SetBoardSize", SetBoardSize);
+BootStrapList.set("CreateEventForTilesWithZeroAdjacentCheckQueue", CreateEventForTilesWithZeroAdjacentCheckQueue)
+BootStrapList.set("GenerateBombs", GenerateBombs);
+BootStrapList.set("CheckIfIndexIsBomb", CheckIfIndexIsBomb);
+BootStrapList.set("GetAdjacentNumber", GetAdjacentNumber);
+BootStrapList.set("returnGameStatus", returnGameStatus);
 
-const SetBoardSize = ({ height, width }) => {
-    MineSweeper.setHeightWidth({ height: height, width: width }) 
-};
 
-const CreateEventForTilesWithZeroAdjacentCheckQueue = () => {
-    MineSweeper.createEventForTilesWithZeroAdjacentCheckQueue();
-};
+// EventStore Functions
+const SendTileUncoveredEventToEventStore = ({ Event }) => {EventStore.addLatestEventToProjectionManager({Event: Event});};
+BootStrapList.set("SendTileUncoveredEventToEventStore", SendTileUncoveredEventToEventStore);
 
-const SendTileUncoveredEventToEventStore = ({ Event }) => {
-    EventStore.addLatestEventToProjectionManager({Event: Event});
-};
 
-const ApplyEventToProjectionManager = ({ Event }) => {
-    ProjectionManager.applyEvents({ Event: Event });
-}
+// ProjectionManager Functions
+const ApplyEventToProjectionManager = ({ Event }) => {ProjectionManager.applyEvents({ Event: Event });};
+const ReturnProjectionSpecialMethod = ({ ProjectionType, MethodName, Data }) => {return ProjectionManager.callProjectionSpecialMethod({ ProjectionType: ProjectionType, MethodName: MethodName, Data: Data });};
+BootStrapList.set("ApplyEventToProjectionManager", ApplyEventToProjectionManager);
+BootStrapList.set("ReturnProjectionSpecialMethod", ReturnProjectionSpecialMethod);
 
-const ReturnProjectionSpecialMethod = ({ ProjectionType, MethodName, Data }) => { 
-    return ProjectionManager.callProjectionSpecialMethod({ ProjectionType: ProjectionType, MethodName: MethodName, Data: Data });
-}
-
-const GenerateBombs = ({ amount }) => {
-    return MineSweeper.GenerateBomb({amount: amount});
-}
-
-const CheckIfIndexIsBomb = ({ index }) => {
-    return MineSweeper.isTileUncoveredBomb({ index:index })
-};
-
-const GetAdjacentNumber = ({ index }) => {
-    return MineSweeper.getAdjacentNumber({ index: index });
-}
-
-const returnGameStatus = () => {
-    return MineSweeper.returnGameStatus();
-}
-
+// Uses all
 const ResetEntireData = () => {
     EventStore.clearEvents();
     ProjectionManager.clearEvents();
     MineSweeper.resetGame();
-    console.log("DELETED SUCCESSFULLY")
-}
-
-BootStrapList.set("TilePressed", TilePressed);
-BootStrapList.set("CreateFlagEvent", CreateFlagEvent);
-BootStrapList.set("SetBoardSize", SetBoardSize);
-BootStrapList.set("SendTileUncoveredEventToEventStore", SendTileUncoveredEventToEventStore);
-BootStrapList.set("ApplyEventToProjectionManager", ApplyEventToProjectionManager);
-BootStrapList.set("ReturnProjectionSpecialMethod", ReturnProjectionSpecialMethod);
-BootStrapList.set("GenerateBombs", GenerateBombs);
-BootStrapList.set("CheckIfIndexIsBomb", CheckIfIndexIsBomb);
-BootStrapList.set("GetAdjacentNumber", GetAdjacentNumber);
-BootStrapList.set("CreateEventForTilesWithZeroAdjacentCheckQueue", CreateEventForTilesWithZeroAdjacentCheckQueue)
+    console.log("DELETED SUCCESSFULLY")}
 BootStrapList.set("ResetEntireData", ResetEntireData);
-BootStrapList.set("returnGameStatus", returnGameStatus);
-
-
 export default BootStrapList;
 
 
